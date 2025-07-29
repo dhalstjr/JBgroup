5b7a3787b852c2e726ec5db8362b68af0078f917
오민석 <dhalstjr03@gmail.com>
2025-07-29 19:38:17 +0900

    main section3에 swiper 라이브러리 CSS와 js를 가져와서. section3에 있는 slide구현. -> freemode와 slidesPerview만 사용해서 구현, freemode에 대해서 알았음.

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
         // gsap.set()함수는 요소의 초기 스타일 설정하는 역할을 합니다. 애니메이션이 시작하기 전에 요소의 시작 상태를 정의하는데 사용된다.
         gsap.set(visualImg, {
-          yPercent: progress * 50,
+          // yPercent: progress * 50,
         });
       },
     });
@@ -62,7 +62,7 @@ window.addEventListener("DOMContentLoaded", function () {
       start: "bottom 80%", // scroller가 Section1에 80% 지점에서 있고,  bottom에 있는  start를 트리거 시 애니메이션 시작.
       end: "bottom top",
 
-      markers: true,
+      // markers: true,
 
       // onUpdate는 gsap에서 애니메이션이 업데이트 될 때마다 실행되는 콜백함수
       // progress는 현재 진행률값을 인자로 받아, 해당값에 따라 추가적인 동작을 수행할 수 있다.
@@ -99,6 +99,100 @@ window.addEventListener("DOMContentLoaded", function () {
       onEnterBack: () => {},
       // onLeaveBack은 트리거의 시작점보다 위로(스크롤을 더 위로)올라가서 트리거에 빠져나갈때 실행. 비디오를 실행bb
       onLeaveBack: () => {},
+    });
+  }
+
+  // 첫번째 섹션의 효과 border-radius를 적영해게;
+
+  // 1-2 첫번째 섹션의 텍스트와 위젯 설정. (gsap- splitText가 필요해 다음에.)
+
+  // 두번째 섹션에 대한 js (visual 핀, 이미지 yPercent, 그리고 반응형 Swiper 처리)
+  // 2-1 visual 핀
+  // 두번째 섹션에 필요한 변수 저장
+  const Section2 = document.querySelector(".section2");
+  const Visual = Section2.querySelector(".visual-bg");
+  console.log(Section2, Visual);
+
+  if (Section2) {
+    ScrollTrigger.create({
+      trigger: Section2,
+      start: "top bottom",
+      end: "bottom bottom",
+
+      // markers: true,
+
+      // onUpdate는 애니메이션이 업데이트될 때 마다 실행하는 콜백함수.
+      onUpdate: ({ progress }) => {
+        // 지금 이 코드에서 하고싶은 건 스크롤 진행도에 따라 .visual-bg가 y축으로 움직이게 하고싶은 것이다.
+        //(1 - progress)는 progress가 커질수록 1에 가까워지니까, 처음에는 1(아직 안 내려갔을 때) 끝나면 0(다 내려갔을 때) 그러니까, 처음엔 많이. 끝에서 적게 움직이게 하고싶은 것이다.
+        // 이해가 안가는 부분을 물어봤어 progress에 진행률이 달라지는 것 같은 느낌이 들어서 물어봤더니, 1 - progress는 0~1의 진행률을 반대로 1~0으로 바꾼 코드라는 것.
+        // 지금 우리가 구현하고자 하는 애니메이션은 스크롤이 진행될수록 값이 줄어들어야 하는데, progress는 스크롤이 진행될수록 값이 증가하다 보니 반대로 만들기 위해서 1 - progress 라는 문법을 사용한 것이다.
+        // 스크롤이 내려갈수록 값이 줄어드는 효과를 만들고 싶은 경우 이렇게 사용한다.
+        // 왜 * -20을 사용했는가. yPercent에 음수를 곱하면 위쪽으로 이동하게 됩니다. -20이라는 숫자는 디자인적으로 적당한 이동 범위를 설정한 것이라고 하는데, 내가 보기엔 계산해서 넣은 값 같다
+        // 즉, 시작할 때 (1-0) * -20 = -20 -> 배경을 20% 위에 위치 , 끝날 때는 (1-1) * 20 = 0 -> 제자리로 돌아옴 : 스크롤을 내릴수록 배경이 위에서 아래로 부드럽게 내려오는 듯한 효과
+        // 스크롤 도중 배경이 살짝 따라 내려오는 효과를 주기 위해.
+        gsap.set(Section2.querySelector(".visual-bg"), {
+          yPercent: (1 - progress) * -20,
+        });
+      },
+    });
+
+    // 배경 도형에 애니메이션 효과 - 클래스를 추가하는 방식으로 구현(이미지를 넣고, top과 left 위치를 변경하고, rotate를 사용해 이미지 도형을 움직이는 효과)
+    ScrollTrigger.create({
+      trigger: Section2,
+      start: "top 80%",
+      end: "bottom bottom",
+
+      // markers: true,
+
+      onEnter: () => {
+        Section2.querySelector(".visual-bg").classList.add("active");
+      },
+    });
+  }
+
+  // 3. 세번째 섹션 js 구현
+  // 3-1 세번째 섹션에 필요한 변수를 저장
+  const Section3 = document.querySelector(".section3");
+  console.log(Section3);
+
+  // 3-2 main section3에 Swiper 구현
+  if (Section3) {
+    // Swiper를 위해서 슬라이드할 변수를 저장
+    // 그리고 Swiper.js를 사용하기 위해서는 라이브러리를 가져와야함.
+    // Swiper에 있는 CSS를 가져오면 swiper라는 클래스를 준 곳에는 overflow :  hidden이 설정되어있어 item이 4개 이후로 보이지 않지만, hidden을 풀면 보인다.
+    const familySwiper = new Swiper("#main-section .swiper", {
+      freeMode: true,
+      slidesPerView: "auto", // 한 번에 보여줄 슬라이드 개수를 지정하는 기능.
+      // 그렇다면 "auto" 옵션의 기능은 슬라이드 컨테이너의 너비에 맞춰 슬라이드들이 자동으로 크기가 조정되도록 설정
+    });
+  }
+
+  // 4. 네번째 섹션 js 구현
+  // 4-1 세번째 섹션에 필요한 변수를 저장
+  const Section4 = document.querySelector(".section4");
+  console.log(Section4);
+
+  // 4-2 세번째 섹션 visual 핀 처리
+  if (Section4) {
+    ScrollTrigger.create({
+      trigger: Section4,
+      start: "top top",
+      end: "bottom bottom",
+
+      // section4에 잇는 visual에 pin을 걸고 봤더니, section4에 있는 이미지가 스크롤에 의해서 내려가는 듯한 효과를 주는데, pin만 걸었는데도, 어떻게 그럴 수 있는지 HTML과 CSS에 연관해서 물어보았어
+      // pin 동작 원리와 스크롤의 자연 흐름 때문이라고 한다.
+      // section4에 visual이 pin이 고정된 상태로 스크롤을 따라 움직이지 않지만, 그 하위 요소인 sec4-bg가 여전히 배경 이미지로 존재하고 있다
+      // CSS에서 sec4-bg는  absolute로, visual 기준으로 절대 위치 지정되고, 그래서 고정된 visual 안에서 배경처럼 보이는 것이고, 스크롤이 내려가면 주변 콘텐츠는 계속 움직이지만, sec4-bg는 그대로 있기에 내려가는 것처럼 보이게 되는 것이다.
+      // 그리고 pinSpacing -> true와 false의 차이점은 기본값 true는 visual이 고정되는 동안 그만큼의 공간을 차지 -> 그만큼 콘텐츠가 더 길어짐
+      // false는 고정되지만 공간을 차지하지 않음. -> 바로 다음 콘텐츠가 겹치며 올라옴.
+      // 결과적으로 pinSpacing이 false면 시각적으로 sec4-bg가 더 오래 남아있는 것처럼 보여서 더 많이 내려가는 것처럼 보이게 됌 ->  실제로 내려간 것이 아니라. 고정된 상태에서 주변 콘텐츠가 위로 올라와서 생기는 착시현상.
+      // 결론은 .visual이 고정되고, 그 안의 sec4-bg는 고정된 컨테이너에 위치. 화면은 계속 스크롤 되고, 다른 요소들이 올라오게 되니 상대적으로 sec4-bg가 점점 아래로 내려가는 것처럼 보이는 착시 효과 발생.
+
+      pin: Section4.querySelector(".visual"),
+      pinSpacing: false, // 요소가 고정될 때 (pin : true)생기는 공간을 다른 요소들이 침범하지 않도록 막아주는 기능
+
+      markers: true,
     });
   }
 });
